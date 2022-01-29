1bf1a253cad0b62e99f2b9ebc153534873133bb2
Junior <junior.jaava@gmail.com>
2022-01-29 18:32:44 +0100

    Styling the selected route

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -21,7 +21,7 @@ export class ProductEditComponent implements OnInit{
               private route: ActivatedRoute,
               private router: Router) { }
 
-  ngOnInit(): void {  // ngOnIt is executaded just once 
+  ngOnInit(): void {  // ngOnIt is executaded just once
     this.route.data.subscribe(data => {
       const resolvedData: ProductResolved = data['resolvedData'];
       this.errorMessage = resolvedData.error;
@@ -57,6 +57,15 @@ export class ProductEditComponent implements OnInit{
     }
   }
 
+  isValid(path?: string): boolean {
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+  }
+
   saveProduct(): void {
     if (true === true) {
       if (this.product.id === 0) {
@@ -82,5 +91,27 @@ export class ProductEditComponent implements OnInit{
 
     // Navigate back to the product list
     this.router.navigate(['/products']);
+  }
+
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName &&
+      this.product.productName.length >= 3 &&
+      this.product.productCode) {
+      this.dataIsValid['info'] = true;
+    } else {
+      this.dataIsValid['info'] = false;
+    }
+
+    // 'tags' tab
+    if (this.product.category &&
+      this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true;
+    } else {
+      this.dataIsValid['tags'] = false;
+    }
   }
 }
