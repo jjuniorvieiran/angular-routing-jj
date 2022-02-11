116d65ce838acf93ca364c1cf0d0ec6ae7d91c48
Junior <junior.jaava@gmail.com>
2022-02-11 18:28:03 +0100

    canActivate guard

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -11,6 +11,7 @@ import {ProductResolver} from './product-resolver.service';
 import {SharedModule} from '../shared/shared.module';
 import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
 import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
+import{AuthGuard} from '../user/auth.guard';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import {ProductEditTagsComponent} from './product-edit/product-edit-tags.compone
     RouterModule.forChild([
       {
         path: 'products',
+        canActivate: [AuthGuard],
         children: [
           {
             path: '',
