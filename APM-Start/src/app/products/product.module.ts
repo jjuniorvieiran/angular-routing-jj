b666860d51513fd7d2e9172e270ab3f0796462e9
Junior <junior.jaava@gmail.com>
2022-03-06 15:36:15 +0100

    + preparing to lazy loading - adding lazy loading in app-routing.modules

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,55 +1,49 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {ProductListComponent} from './product-list.component';
-import {ProductDetailComponent} from './product-detail.component';
-import {ProductEditComponent} from './product-edit/product-edit.component';
-import {ProductEditGuard} from './product-edit/product-edit.guard';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditGuard } from './product-edit/product-edit.guard';
 
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-import {ProductResolver} from './product-resolver.service';
+import { ProductResolver } from './product-resolver.service';
 
-import {SharedModule} from '../shared/shared.module';
-import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
-import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
-import{AuthGuard} from '../user/auth.guard';
+import { SharedModule } from '../shared/shared.module';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       {
-        path: 'products',
-        canActivate: [AuthGuard],
+        path: '',
+        component: ProductListComponent
+      },
+      { path: ':id', component: ProductDetailComponent, resolve: { resolvedData: ProductResolver } },
+      {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        canDeactivate: [ProductEditGuard],
+        resolve: { resolvedData: ProductResolver },
         children: [
           {
             path: '',
-            component: ProductListComponent
+            redirectTo: 'info',
+            pathMatch: 'full'
           },
-          {path: ':id', component: ProductDetailComponent, resolve: {resolvedData: ProductResolver}},
           {
-            path: ':id/edit',
-            component: ProductEditComponent,
-            canDeactivate: [ProductEditGuard],
-            resolve: {resolvedData: ProductResolver},
-            children: [
-              {
-                path: '',
-                redirectTo: 'info',
-                pathMatch: 'full'
-              },
-              {
-                path: 'info',
-                component: ProductEditInfoComponent
-              },
-              {
-                path: 'tags',
-                component: ProductEditTagsComponent
-              }
-            ]
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
           }
         ]
       }
+
     ])
   ],
   declarations: [
